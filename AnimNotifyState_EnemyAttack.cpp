3e3dd49de6d3fb3907f2fa82e38ba54335f0e7bf
mcran127 <112192289+mcran127@users.noreply.github.com>
2025-06-19 11:00:09 -0600

    Most interesting notify state

---
+++ AnimNotifyState_EnemyAttack.cpp
@@ -0,0 +1,153 @@
+﻿// Copyright Brigham Young University. All Rights Reserved.
+
+#include "AnimNotifyState_EnemyAttack.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Engine/SkeletalMesh.h"
+#include "Engine/OverlapResult.h"
+#include "Misc/DataValidation.h"
+#include "PhysicsEngine/PhysicsAsset.h"
+#include "PhysicsEngine/SkeletalBodySetup.h"
+#include "Y25/Enemies/BaseEnemy.h"
+
+UAnimNotifyState_EnemyAttack::UAnimNotifyState_EnemyAttack()
+{
+#if WITH_EDITORONLY_DATA
+	bShouldFireInEditor = true;
+#endif
+}
+
+void UAnimNotifyState_EnemyAttack::NotifyBegin(
+	USkeletalMeshComponent* MeshComp,
+	UAnimSequenceBase* Animation,
+	const float TotalDuration,
+	const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+	//Setup who's getting hit
+	BodyInstance = nullptr;
+	HitInstances.Empty();
+	HitCharacters.Empty();
+
+	//Make sure you have the mesh and physics body
+	if (!MeshComp)
+	{
+		return;
+	}
+
+	if (PhysicsBody.IsNone())
+	{
+		return;
+	}
+
+	//Make body instance
+	BodyInstance = MeshComp->GetBodyInstance(PhysicsBody);
+}
+
+void UAnimNotifyState_EnemyAttack::NotifyTick(
+	USkeletalMeshComponent* MeshComp,
+	UAnimSequenceBase* Animation,
+	const float FrameDeltaTime,
+	const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime, EventReference);
+
+	//Check mesh and bodyinstance
+	if (!MeshComp || !BodyInstance)
+	{
+		return;
+	}
+
+	//Check overlap
+	const FTransform Transform = MeshComp->GetSocketTransform(PhysicsBody, RTS_World);
+
+	TArray<FOverlapResult> Results;
+	BodyInstance->OverlapMulti(
+		Results,
+		MeshComp->GetWorld(),
+		nullptr,
+		Transform.GetLocation(),
+		Transform.GetRotation(),
+		ECC_Pawn,
+		FComponentQueryParams::DefaultComponentQueryParams,
+		FCollisionResponseParams::DefaultResponseParam);
+
+	//Get all overlaps
+	for (const FOverlapResult& Result : Results)
+	{
+		//Ignore if already in
+		if (HitInstances.Contains(Result.OverlapObjectHandle))
+		{
+			continue;
+		}
+		
+		if (AActor* Actor = Result.GetActor())
+		{
+			if (HitCharacters.Contains(Actor))
+			{
+				continue;
+			}
+			HitCharacters.Add(Actor);
+
+			//Call damage in enemy
+			if (ABaseEnemy* BaseEnemy = Cast<ABaseEnemy>(MeshComp->GetOwner()))
+			{
+				BaseEnemy->Attacking(Actor);
+			}
+		}
+	}
+}
+
+void UAnimNotifyState_EnemyAttack::NotifyEnd(
+	USkeletalMeshComponent* MeshComp,
+	UAnimSequenceBase* Animation,
+	const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+
+	//clear body instance and sets
+	BodyInstance = nullptr;
+	HitInstances.Empty();
+	HitCharacters.Empty();
+}
+
+#if WITH_EDITOR
+TArray<FName> UAnimNotifyState_EnemyAttack::GetPhysicsBodyOptions() const
+{
+	TArray<FName> Names;
+
+	if (PhysicsAsset)
+	{
+		for (const USkeletalBodySetup* SkeletalBodySetup : PhysicsAsset->SkeletalBodySetups)
+		{
+			if (SkeletalBodySetup)
+			{
+				Names.Add(SkeletalBodySetup->BoneName);
+			}
+		}
+	}
+
+	return Names;
+}
+
+EDataValidationResult UAnimNotifyState_EnemyAttack::IsDataValid(FDataValidationContext& Context) const
+{
+	EDataValidationResult Result = Super::IsDataValid(Context);
+
+	if (PhysicsBody.IsNone())
+	{
+		Context.AddError(INVTEXT("Physics body is not set."));
+		Result = EDataValidationResult::Invalid;
+	}
+	else
+	{
+		if (!GetPhysicsBodyOptions().Contains(PhysicsBody))
+		{
+			Context.AddError(INVTEXT("Physics body could not be found in the physics asset."));
+			Result = EDataValidationResult::Invalid;
+		}
+	}
+
+	return Result;
+}
+#endif
---
+++ AnimNotifyState_EnemyAttack.h
@@ -0,0 +1,61 @@
+﻿// Copyright Brigham Young University. All Rights Reserved.
+
+#pragma once
+
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "Engine/ActorInstanceHandle.h"
+#include "PhysicsEngine/BodyInstance.h"
+#include "Y25/Player/MainCharacter.h"
+
+#include "AnimNotifyState_EnemyAttack.generated.h"
+
+UCLASS()
+class Y25_API UAnimNotifyState_EnemyAttack : public UAnimNotifyState
+{
+	GENERATED_BODY()
+
+public:
+	UAnimNotifyState_EnemyAttack();
+
+	virtual void NotifyBegin(
+		USkeletalMeshComponent* MeshComp,
+		UAnimSequenceBase* Animation,
+		float TotalDuration,
+		const FAnimNotifyEventReference& EventReference) override;
+
+	virtual void NotifyTick(
+		USkeletalMeshComponent* MeshComp,
+		UAnimSequenceBase* Animation,
+		float FrameDeltaTime,
+		const FAnimNotifyEventReference& EventReference) override;
+
+	virtual void NotifyEnd(
+		USkeletalMeshComponent* MeshComp,
+		UAnimSequenceBase* Animation,
+		const FAnimNotifyEventReference& EventReference) override;
+
+protected:
+#if WITH_EDITORONLY_DATA
+	UPROPERTY(EditAnywhere, Category = "Config")
+	TObjectPtr<UPhysicsAsset> PhysicsAsset;
+#endif
+
+	UPROPERTY(EditAnywhere, Category = "Config", meta = (GetOptions = GetPhysicsBodyOptions))
+	FName PhysicsBody;
+
+private:
+	FBodyInstance* BodyInstance = nullptr;
+
+	UPROPERTY(Transient)
+	TSet<FActorInstanceHandle> HitInstances;
+
+	UPROPERTY(Transient)
+	TSet<TObjectPtr<AActor>> HitCharacters;
+
+#if WITH_EDITOR
+	UFUNCTION()
+	TArray<FName> GetPhysicsBodyOptions() const;
+
+	virtual EDataValidationResult IsDataValid(FDataValidationContext& Context) const override;
+#endif
+};
